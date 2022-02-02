c09afc4e97e454166f5d46c4cfabb90d921e8652
NicolasGarciaRodriguez <nicogarcia.ro97@gmail.com>
2022-02-02 12:39:33 +0100

    02-02-2022

--- password_generator.py
+++ password_generator.py
@@ -1,8 +1,41 @@
 import os
 import random as rd
+from time import sleep
 
 savedpasswords = []
+titles_list = []
 
+
+def save_file():
+    file_name = input("Add your file name\n"
+                      "Respone: ")
+    save = open(file_name + ".txt", "w")
+    save.write("\n".join(savedpasswords))
+    save.close()
+    print("File sucessfully created")
+    sleep(2)
+    os.system("cls")
+    main()
+
+
+def add_title_name(password):
+    title = input("\n\n\nEnter your title...\n\n\n"
+                  "Respuesta: ")
+
+    while title.lower() in titles_list:
+        print("A password with this name, already exists")
+        print("Saved passwords: {}".format(savedpasswords))
+        add_title_name(password)
+    if len(title) <= 0 or title == " ":
+        print("Name can not be empty")
+        add_title_name(password)
+
+    titles_list.append(title.lower())
+    savedpasswords.append(title.lower() + ": " + password)
+    print("\n\n\nSaved passwords: {}\n\n\n".format(savedpasswords))
+    input("Enter to go back...")
+    os.system("cls")
+    main()
 
 def show_password(password):
     print("\n\n\nYour random password: {}\n\n\n".format(password))
@@ -23,7 +56,6 @@ def password_generator():
 def save_password(password):
     savepassword = None
     save_title = None
-    title = None
     while savepassword != "Y" and savepassword != "N":
         savepassword = input("\n\n\nDo you want to save your password? (Y/N)\n\n\n"
                              "Respuesta: ")
@@ -42,23 +74,24 @@ def save_password(password):
         os.system("cls")
         main()
     os.system("cls")
-    title = input("\n\n\nEnter your title...\n\n\n"
-                  "Respuesta: ")
-    savedpasswords.append(title + ": " + password)
-    print ("\n\n\nSaved passwords: {}\n\n\n".format (savedpasswords))
-    input ("Enter to go back...")
-    os.system("cls")
-    main()
+    add_title_name(password)
 
 
 
 def main():
     if len(savedpasswords) > 0:
         print("\n\n\nSaved passwords {}".format(savedpasswords))
-    print("\n\n\nWelcome to the password generator\n\n\n")
-    input("\n\n\nPress Enter to generate a new password...\n\n\n")
-    os.system("cls")
-    password_generator()
-
+        print ("\n\n\nWelcome to the password generator\n\n\n")
+        question = input ("\n\n\nPress Enter to generate a new password or (Y) to create a file...\n\n\n")
+        if question == "Y":
+            save_file()
+        os.system("cls")
+        password_generator()
+        save_file()
+    else:
+        print("\n\n\nWelcome to the password generator\n\n\n")
+        input("\n\n\nPress Enter to generate a new password...\n\n\n")
+        os.system("cls")
+        password_generator()
 
 main()
